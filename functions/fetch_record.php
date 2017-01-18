7c0ff6ecfa2dbc8b6db39c28af3a38fc66b25fef
Tim Westland <tim@172-222-0-168.res.bhn.net>
2017-01-18 16:53:57 +0100

    Commit mac verwijder onnodige bestander login pagina toegevoegd

--- functions/fetch_record.php
+++
@@ -1,9 +0,0 @@
-<?php
-//Include database connection here
-if($_POST['id']) {
-    $id = $_POST['id'];
-    // Run the Query
-    // Fetch Records
-    // Echo the data you want to show in modal
- }
-?>
